c68c69fdcdad971a193a7639acae3ab6edf5cc0c
MontagneIhsane <ihsane.montagne@gmail.com>
2024-01-29 15:13:37 +0100

    update

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log('Hello World!');
+document.addEventListener('DOMContentLoaded', () => {
+    // État et variable du jeu
+    const  bird = document.querySelector('.bird');
+    const gameDisplay = document.querySelector('.game-display');
+    const ground = document.querySelector('.ground-moving');
+
+    let birdLeft = 220;
+    let birdBttom = 100;
+    let gravity = 3;
+    let isGameOver = false;
+    let score = 0;
+
+
+    //Fonctions du jeu 
+})
